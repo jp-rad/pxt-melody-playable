d475c72792591b5fafe349647c134f56b8565089
jp-rad <jp-rad@example.com>
2023-12-15 13:25:21 +0000

    playable

--- test.ts
+++ test.ts
@@ -1,13 +1,26 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+/**
+ * tests go here; this will not be compiled when this package is used as an extension.
+ */
+// play
 input.onButtonPressed(Button.A, function () {
-    a = custom.bar(a)
+    music.play(melody.melodyPlayable(0), music.PlaybackMode.UntilDone)
 })
-input.onButtonPressed(Button.B, function () {
-    a = 0
+// declare
+melody.declareMelody(0, function () {
+    for (let index = 0; index < 2; index++) {
+        melody.scale(262, BeatFraction.Whole)
+        melody.scale(294, BeatFraction.Whole)
+        melody.scale(330, BeatFraction.Whole)
+        melody.rest(BeatFraction.Whole)
+    }
+    melody.scale(392, BeatFraction.Whole)
+    melody.scale(330, BeatFraction.Whole)
+    melody.scale(294, BeatFraction.Whole)
+    melody.scale(262, BeatFraction.Whole)
+    melody.scale(294, BeatFraction.Whole)
+    melody.scale(330, BeatFraction.Whole)
+    melody.scale(294, BeatFraction.Whole)
+    melody.rest(BeatFraction.Whole)
 })
-let a = 0
-a = 0
-basic.forever(function () {
-    basic.showNumber(a)
-    basic.pause(100)
-})
+// icon
+basic.showIcon(IconNames.EighthNote)
